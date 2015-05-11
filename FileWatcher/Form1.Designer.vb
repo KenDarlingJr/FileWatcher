fd0a56044f399acde18c930e6ffc173208a15283
Ken Darling <kdarling@pafcorp.com>
2015-05-11 09:24:14 -0400

    1st Commit

---
+++ FileWatcher/Form1.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.txtWatchPath = New System.Windows.Forms.TextBox()
+        Me.txtWatchFile = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(3, 3)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(181, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'ListView1
+        '
+        Me.ListView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListView1.BackColor = System.Drawing.Color.Black
+        Me.ListView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1})
+        Me.ListView1.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListView1.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.ListView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None
+        Me.ListView1.Location = New System.Drawing.Point(1, 96)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(572, 279)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Width = 553
+        '
+        'txtWatchPath
+        '
+        Me.txtWatchPath.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtWatchPath.BackColor = System.Drawing.Color.Black
+        Me.txtWatchPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtWatchPath.Font = New System.Drawing.Font("Consolas", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtWatchPath.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.txtWatchPath.Location = New System.Drawing.Point(57, 12)
+        Me.txtWatchPath.Name = "txtWatchPath"
+        Me.txtWatchPath.Size = New System.Drawing.Size(504, 22)
+        Me.txtWatchPath.TabIndex = 1
+        '
+        'txtWatchFile
+        '
+        Me.txtWatchFile.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtWatchFile.BackColor = System.Drawing.Color.Black
+        Me.txtWatchFile.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtWatchFile.Font = New System.Drawing.Font("Consolas", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtWatchFile.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.txtWatchFile.Location = New System.Drawing.Point(57, 38)
+        Me.txtWatchFile.Name = "txtWatchFile"
+        Me.txtWatchFile.Size = New System.Drawing.Size(504, 22)
+        Me.txtWatchFile.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Consolas", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 14)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Path"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Consolas", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.Label2.Location = New System.Drawing.Point(12, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 14)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "File"
+        '
+        'btnStart
+        '
+        Me.btnStart.BackColor = System.Drawing.Color.Black
+        Me.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnStart.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStart.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.btnStart.Location = New System.Drawing.Point(57, 67)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 5
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.Black
+        Me.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnClear.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.btnClear.Location = New System.Drawing.Point(138, 67)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(573, 374)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtWatchFile)
+        Me.Controls.Add(Me.txtWatchPath)
+        Me.Controls.Add(Me.ListView1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "File Watch"
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents FileSystemWatcher1 As System.IO.FileSystemWatcher
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtWatchFile As System.Windows.Forms.TextBox
+    Friend WithEvents txtWatchPath As System.Windows.Forms.TextBox
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents btnStart As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+
+End Class
---
+++ FileWatcher/Form1.vb
@@ -0,0 +1,85 @@
+﻿Imports System.Media
+
+Public Class Form1
+
+    Dim WatchPath As String
+    Dim WatchFile As String
+    'Color.Chartreuse
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtWatchPath.Text = My.Settings.WatchPath
+        txtWatchFile.Text = My.Settings.WatchFile
+
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        My.Settings.WatchPath = txtWatchPath.Text
+        My.Settings.WatchFile = txtWatchFile.Text
+
+        WatchFile = My.Settings.WatchFile
+
+        FileSystemWatcher1.Path = My.Settings.WatchPath
+        'Wildcards will work on filtere
+        '   *.*             All files (default). An empty string ("") also watches all files.
+        '   *.txt           All files with a "txt" extension.
+        '   MyReport.Doc    Watches only MyReport.doc
+        '   *recipe.doc     All files ending in "recipe" with a "doc" extension.
+        '   win*.xml        All files beginning with "win" with an "xml" extension.
+        '   Sales*200?.xls  Matches the following: Sales July 2001.xls, Sales Aug 2002.xls, Sales March 2004.xls
+        '                   but does not match: Sales Nov 1999.xls
+
+        FileSystemWatcher1.Filter = WatchFile
+
+        ' add event handlers
+        AddHandler FileSystemWatcher1.Changed, AddressOf OnChanged
+        AddHandler FileSystemWatcher1.Created, AddressOf OnCreated
+        AddHandler FileSystemWatcher1.Deleted, AddressOf OnDeleted
+
+        ListView1.Items.Add("File Watcher Started!")
+
+
+    End Sub
+
+    Private Sub OnChanged(sender As Object, e As IO.FileSystemEventArgs)
+
+        Dim listViewItem = New ListViewItem
+        listViewItem.Text = ("File Changed : " & e.Name & " " & DateAndTime.Now)
+
+        listViewItem.ForeColor = Color.Yellow
+        ListView1.Items.Add(listViewItem)
+        ListView1.EnsureVisible(ListView1.Items.Count - 1)
+        ListView1.Update()
+
+    End Sub
+
+    Private Sub OnCreated(sender As Object, e As IO.FileSystemEventArgs)
+
+        Dim listViewItem = New ListViewItem
+        listViewItem.Text = ("File Created : " & e.Name & " " & DateAndTime.Now)
+
+        listViewItem.ForeColor = Color.DeepSkyBlue
+        ListView1.Items.Add(listViewItem)
+        ListView1.EnsureVisible(ListView1.Items.Count - 1)
+        ListView1.Update()
+
+    End Sub
+
+    Private Sub OnDeleted(sender As Object, e As IO.FileSystemEventArgs)
+
+        Dim listViewItem = New ListViewItem
+        listViewItem.Text = "File Deleted : " & e.Name & " " & DateAndTime.Now
+
+        listViewItem.Font = New Font(ListView1.Font, FontStyle.Italic)
+        listViewItem.ForeColor = Color.Red
+        ListView1.Items.Add(listViewItem)
+        ListView1.EnsureVisible(ListView1.Items.Count - 1)
+        ListView1.Update()
+        SystemSounds.Beep.Play()
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ListView1.Items.Clear()
+    End Sub
+End Class
